04e34939cb9c18dd65c42de8c93c0f2a65fc5ba7
Kanishka Kapoor <kapoor.kanishka@gmail.com>
2021-09-14 12:34:53 +1000

    Made one more test to test the number of completed tasks.

--- script.test.js
+++ script.test.js
@@ -3,11 +3,15 @@ const { getCompletedTasks, getTodoTasks } = require("./script");
 
 
 //This tests the counter when "completed" parameter of json is true.
-it("get number of tasks completed", async() => {
+it("get number of tasks completed 1", async() => {
     const completedTasks = await getCompletedTasks(true, 8);
     expect(completedTasks).toEqual(9);
 })
 
+it("get number of tasks completed 2", async() => {
+    const completedTasks = await getCompletedTasks(false, 2);
+    expect(completedTasks).toEqual(2);
+})
 //This tests the counter when "completed" parameter of json is false.
 it("get number of tasks left 1", async() => {
     const todoTasks = await getTodoTasks(false, 0);
